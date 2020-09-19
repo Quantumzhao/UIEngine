47845983909c29efeff80814943d6522aa1d570d
Yishan Zhao <yishanzh@umd.edu>
2020-09-18 22:39:43 -0400

    Renamed file

---
+++ UIEngine/Nodes/ExtensionFunctionNode.cs
@@ -0,0 +1,23 @@
+﻿namespace UIEngine.Nodes
+{
+	public abstract class ExtensionFunctionNode : Node
+	{
+		private const string _INVALID_OPERATION_WARNING = "Extension function nodes do not have succession";
+
+		public abstract ObjectNode Invoke();
+		internal protected ExtensionFunctionNode(CollectionNode collection)
+		{
+			SourceCollection = collection;
+		}
+
+		public CollectionNode SourceCollection { get; private set; }
+		internal protected ObjectNode Enumerator { get; set; }
+
+		internal abstract bool DoesSatisfySignature();
+		internal override ObjectNode InstantiateSuccession()
+		{
+			Dashboard.RaiseWarningMessage(this, _INVALID_OPERATION_WARNING);
+			return null;
+		}
+	}
+}
